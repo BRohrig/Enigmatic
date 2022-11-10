05af368a3f37f9fdd4fc4919fb4da0915144e79c
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-10 15:30:56 -0800

    test/feat: enigma initialize

---
+++ lib/enigma.rb
@@ -0,0 +1,8 @@
+class Enigma
+  attr_reader :alphabet
+
+  def initialize
+    @alphabet = ("a".."z").to_a << " "
+  end
+
+end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,7 +1,13 @@
 require './lib/enigma'
 
 RSpec.describe Enigma do
+  let(:enigma) {Enigma.new}
+  let(:letters) {("a".."z").to_a << " "}
 
+  it 'exists and has an alphabet attribute that contains all the letters and a space' do
+    expect(enigma).to be_a(Enigma)
+    expect(enigma.alphabet).to eq(letters)
+  end
 
 
 end
