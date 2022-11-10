092bb4c44051b5c1373f4ddafdb614e0f677631e
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-10 15:37:18 -0800

    test: started shift class testing

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -3,9 +3,12 @@ require './lib/enigma'
 
 RSpec.describe Shift do
   let(:shift) {Shift.new}
+  let(:letters) {("a".."z").to_a << " "}
 
-  it 'is a shift' do
+  it 'is a shift and a subclass of enigma' do
     expect(shift).to be_a(Shift)
+    expect(shift.superclass).to be_a(Enigma)
+    expect(shift.alphabet).to eq(letters)
   end
 
 end
