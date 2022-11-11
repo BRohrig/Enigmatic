fccc5ce2e53a454810e2f4990045bd0fb8003e3e
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-11 11:25:14 -0800

    feat: completed enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,12 +4,22 @@ require './shift'
 
 class Enigma
   def encrypt(message, key, date)
-    shift = Shift.new({:key => key, :date => date}).find_shifts
-    Rotation.new(message).encrypt(shift)
+    shift = Shift.new({:key => key, :date => date})
+    new_message = Rotation.new(message).encrypt(shift.find_shifts)
+    "{ 
+      encryption: => #{new_message},
+      key:        => #{key},
+      date:       => #{Date.parse(shift.date).strftime('%d%m%y')}
+    }"
   end
 
   def decrypt(message, key, date)
-    shift = Shift.new({:key => key, :date => date}).find_shifts
-    Rotation.new(message).decrypt(shift)
+    shift = Shift.new({:key => key, :date => date})
+    new_message = Rotation.new(message).decrypt(shift.find_shifts)
+    "{ 
+      decryption: => #{new_message},
+      key:        => #{key},
+      date:       => #{Date.parse(shift.date).strftime('%d%m%y')}
+    }"
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe Enigma do
   end
 
   it 'can encrypt a message' do
-    expect(enigma.encrypt("I am a happy purple muffin bunny", "12345", "11-Nov-2022")).to eq("bdphteocutdtttimiptvfyuabroxnrbt")
+    expect(enigma.encrypt("I am a happy purple muffin bunny", "12345", "11-Nov-2022")).to eq("{ \n      encryption: => bdphteocutdtttimiptvfyuabroxnrbt,\n      key:        => 12345,\n      date:       => 111122\n    }")
   end
 
   it 'can decrypt a message' do
-    expect(enigma.decrypt("bdphteocutdtttimiptvfyuabroxnrbt", "12345", "11-Nov-2022")).to eq("i am a happy purple muffin bunny")
+    expect(enigma.decrypt("bdphteocutdtttimiptvfyuabroxnrbt", "12345", "11-Nov-2022")).to eq("{ \n      decryption: => i am a happy purple muffin bunny,\n      key:        => 12345,\n      date:       => 111122\n    }")
   end
 
 
