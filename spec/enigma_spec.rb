38a68b5e09815ac1c9e3b567d806784888047eff
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-11 20:09:49 -0800

    fix/test: added test for special characters and implemented

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -16,5 +16,10 @@ RSpec.describe Enigma do
     expect(enigma.decrypt("bdphteocutdtttimiptvfyuabroxnrbt", "12345", "11-Nov-2022")).to eq("{ \n      decryption: => i am a happy purple muffin bunny,\n      key:        => 12345,\n      date:       => 111122\n    }")
   end
 
+  it 'can encrypt/decrypt a message with special characters' do
+    expect(enigma.encrypt("I am a happy purple muffin bunny!", "12345", "11-Nov-2022")).to eq("{ \n      encryption: => bdphteocutdtttimiptvfyuabroxnrbt!,\n      key:        => 12345,\n      date:       => 111122\n    }")
+    expect(enigma.decrypt("bdphteocutdtttimiptvfyuabroxnrbt!", "12345", "11-Nov-2022")).to eq("{ \n      decryption: => i am a happy purple muffin bunny!,\n      key:        => 12345,\n      date:       => 111122\n    }")
+  end
+
 
 end
