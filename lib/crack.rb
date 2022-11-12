20b440154883088e121730df829f12f2e0b75f3a
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-12 15:47:50 -0800

    feat: reverse offsets method

--- lib/crack.rb
+++ lib/crack.rb
@@ -5,4 +5,17 @@ require_relative './shift'
 class Crack < Enigma
   include Shift
 
+  attr_reader :date
+
+  def initialize(date = Date.today)
+    @date = date.to_s
+  end
+
+  def reverse_offsets
+   {:a => -(create_offset[0].to_i),
+    :b => -(create_offset[1].to_i),
+    :c => -(create_offset[2].to_i),
+    :d => -(create_offset[3].to_i)}
+  end
+
 end
--- spec/crack_spec.rb
+++ spec/crack_spec.rb
@@ -1,11 +1,22 @@
 require './lib/crack'
 
 RSpec.describe Crack do
-  let(:crack) {Crack.new}
+  let(:crack) {Crack.new("2022-11-10")}
+
   it 'exists and is a subclass of enigma, and has the shift module loaded' do
     expect(crack).to be_a(Crack)
     expect(crack.class.superclass).to eq(Enigma)
     expect(crack.class.included_modules).to include(Shift)
+    expect(crack.date).to eq("2022-11-10")
+    crack2 = Crack.new
+    expect(crack2.date).to eq(Date.today.to_s)
+  end
+
+  it 'can use the date to find how to reverse the offset' do
+    expect(crack.reverse_offsets).to eq({:a => -8,
+                                        :b => -8,
+                                        :c => -8,
+                                        :d => -4})
   end
 
 end
