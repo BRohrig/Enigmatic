13146055bdded51e43b7b2c8850271692ca61525
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-13 16:31:11 -0800

    feat: cracker works for unknown key

--- lib/crack.rb
+++ lib/crack.rb
@@ -13,35 +13,204 @@ class Crack < Enigma
     @letters  = ("a".."z").to_a << " "
   end
 
-  def reverse_offsets
-   {:a => -(create_offset[0].to_i),
-    :b => -(create_offset[1].to_i),
-    :c => -(create_offset[2].to_i),
-    :d => -(create_offset[3].to_i)}
-  end
-
   def find_last_four
     @message[-4..-1]
   end
 
-  def index_pos(letter)
-    @letters.find_index(letter)
+  def find_space_key_position
+    (@message.length - 4) % 4
   end
 
-  def find_last_indexes
-    find_last_four.each_char.map do |char|
-      index_pos(char)
+  def find_e_key_position
+    (@message.length - 3) % 4
+  end
+
+  def find_n_key_position
+    (@message.length - 2) % 4
+  end
+
+  def find_d_key_position
+    (@message.length - 1) % 4
+  end
+
+  def find_a_shift_position
+    if find_space_key_position == 0
+      -4
+    elsif find_e_key_position == 0
+      -3
+    elsif find_n_key_position == 0
+      -2
+    elsif find_d_key_position == 0
+      -1
     end
   end
 
-  def find_raw_shifts
-    find_last_indexes.each_with_index.flat_map do |num, i|
-      [26, 4, 13, 3].each_with_index.flat_map do |num2, i2|
-        (num - num2) % 27 if i == i2
-      end
-    end.compact
+  def find_b_shift_position
+    if find_space_key_position == 1
+      -4
+    elsif find_e_key_position == 1
+      -3
+    elsif find_n_key_position == 1
+      -2
+    elsif find_d_key_position == 1
+      -1
+    end
   end
 
+  def find_c_shift_position
+    if find_space_key_position == 2
+      -4
+    elsif find_e_key_position == 2
+      -3
+    elsif find_n_key_position == 2
+      -2
+    elsif find_d_key_position == 2
+      -1
+    end
+  end
 
+  def find_d_shift_position
+    if find_space_key_position == 3
+      -4
+    elsif find_e_key_position == 3
+      -3
+    elsif find_n_key_position == 3
+      -2
+    elsif find_d_key_position == 3
+      -1
+    end
+  end
+
+  def find_a_shift
+    if find_a_shift_position == -4
+      @letters.index(@message[find_a_shift_position]) - 26
+    elsif find_a_shift_position == -3
+      @letters.index(@message[find_a_shift_position]) - 4
+    elsif find_a_shift_position == -2
+      @letters.index(@message[find_a_shift_position]) - 13
+    elsif find_a_shift_position == -1
+      @letters.index(@message[find_a_shift_position]) - 3
+    end
+  end
+
+  def find_b_shift
+    if find_b_shift_position == -4
+      @letters.index(@message[find_b_shift_position]) - 26
+    elsif find_b_shift_position == -3
+      @letters.index(@message[find_b_shift_position]) - 4
+    elsif find_b_shift_position == -2
+      @letters.index(@message[find_b_shift_position]) - 13
+    elsif find_b_shift_position == -1
+      @letters.index(@message[find_b_shift_position]) - 3
+    end
+  end
+
+  def find_c_shift
+    if find_c_shift_position == -4
+      @letters.index(@message[find_c_shift_position]) - 26
+    elsif find_c_shift_position == -3
+      @letters.index(@message[find_c_shift_position]) - 4
+    elsif find_c_shift_position == -2
+      @letters.index(@message[find_c_shift_position]) - 13
+    elsif find_c_shift_position == -1
+      @letters.index(@message[find_c_shift_position]) -3
+    end
+  end
+
+  def find_d_shift
+    if find_d_shift_position == -4
+      @letters.index(@message[find_d_shift_position]) - 26
+    elsif find_d_shift_position == -3
+      @letters.index(@message[find_d_shift_position]) - 4
+    elsif find_d_shift_position == -2
+      @letters.index(@message[find_d_shift_position]) - 13
+    elsif find_d_shift_position == -1
+      @letters.index(@message[find_d_shift_position]) -3
+    end
+  end
+    
+
+  def find_a_subkey
+    multiple = (find_a_shift-(create_offset[0].to_i)) % 27
+    x = [0, 0, 0, 0]
+    x.each_with_index.map do |subkey, i|
+      subkey + multiple + (i * 27)
+    end.reject { |val| val > 99 }
+  end
+
+  def find_b_subkey
+    multiple = (find_b_shift-(create_offset[1].to_i)) % 27
+    x = [0, 0, 0, 0]
+    x.each_with_index.map do |subkey, i|
+      subkey + multiple + (i * 27)
+    end.reject { |val| val > 99 }
+  end
+
+  def find_c_subkey
+    multiple = (find_c_shift-(create_offset[2].to_i)) % 27
+    x = [0, 0, 0, 0]
+    x.each_with_index.map do |subkey, i|
+      subkey + multiple + (i * 27)
+    end.reject { |val| val > 99 }
+  end
+
+  def find_d_subkey
+    multiple = (find_d_shift-(create_offset[3].to_i)) % 27
+    x = [0, 0, 0, 0]
+    x.each_with_index.map do |subkey, i|
+      subkey + multiple + (i * 27)
+    end.reject { |val| val > 99 }
+  end
+
+  def figure_out_key
+    key = [find_a_key, find_b_key, find_c_key, find_d_key]
+    key.reduce([]) { |num, key| num | key.split }.join(' ').delete(" ").squeeze
+  end
+
+  def find_a_key
+    stringify_them("a").find do |string|
+      stringify_them("b").find do |string2|
+        string[1] == string2[0]
+      end
+    end
+  end
+
+  def find_b_key
+    stringify_them("b").find do |string3|
+      find_a_key[1] == string3[0]
+    end
+  end
+
+  def find_c_key
+    stringify_them("c").find do |string4|
+      find_b_key[1] == string4[0]
+    end
+  end
+
+  def find_d_key
+    stringify_them("d").find do |string5|
+      find_c_key[1] == string5[0]
+    end
+  end
+
+  def stringify_them(arg)
+    if arg == "a"
+      find_a_subkey.map do |num|
+        num.to_s.rjust(2, "0")
+      end
+    elsif arg == "b"
+      find_b_subkey.map do |num|
+        num.to_s.rjust(2, "0")
+      end
+    elsif arg == "c"
+      find_c_subkey.map do |num|
+        num.to_s.rjust(2, "0")
+      end
+    elsif arg == "d"
+      find_d_subkey.map do |num|
+        num.to_s.rjust(2, "0")
+      end
+    end
+  end
 
 end
--- spec/crack_spec.rb
+++ spec/crack_spec.rb
@@ -1,10 +1,10 @@
 require './lib/crack'
 
 RSpec.describe Crack do
-  let(:crack) {Crack.new("crack is whack end", "2022-11-10")}
+  # let(:crack) {Crack.new("crack is whack end", "2022-11-10")}
   let(:crack2) {Crack.new("ipyqpkvstddzddawt!mmpgmfspmcpemyponc?krse", "2022-11-12")}
 
-  it 'exists and is a subclass of enigma, and has the shift module loaded' do
+  xit 'exists and is a subclass of enigma, and has the shift module loaded' do
     expect(crack).to be_a(Crack)
     expect(crack.class.superclass).to eq(Enigma)
     expect(crack.class.included_modules).to include(Shift)
@@ -13,29 +13,48 @@ RSpec.describe Crack do
     expect(crack2.date).to eq(Date.today.to_s)
   end
 
-  it 'can use the date to find how to reverse the offset' do
-    expect(crack.reverse_offsets).to eq({:a => -8,
-                                        :b => -8,
-                                        :c => -8,
-                                        :d => -4})
-  end
-
-  it 'can find the last four characters of a message' do
+  xit 'can find the last four characters of a message' do
     expect(crack.find_last_four).to eq(" end")
+    expect(crack2.find_last_four).to eq("krse")
   end
 
-  it 'can find the index positions of the last four' do
-    expect(crack.find_last_indexes).to eq([26, 4, 13, 3])
-  end 
-
-  it 'can find the index differences between the last four and the expected last four' do
-    expect(crack.find_raw_shifts).to eq([0, 0, 0, 0])
-    expect(crack2.find_raw_shifts).to eq([11, 13, 5, 1])
+  xit 'can find the a shift' do
+    # expect(crack.find_a_shift).to eq(-9)
+    expect(crack2.find_a_shift).to eq(1)
   end
 
-  xit 'can find all possible options for the A key' do
-    expect(crack.find_a_key).to eq([0, 27, 54, 81])
-    expect(crack2.find_a_key).to eq([11, 38, 65, 99])
+  it 'can find all possible options for the a subkey' do
+    # expect(crack.find_a_subkey).to eq([19, 46, 73])
+    expect(crack2.find_a_subkey).to eq([20, 47, 74])
+  end
+
+  xit 'can find all possible options for the b subkey' do
+    # expect(crack.find_b_subkey).to eq([9, 36, 63, 90])
+    expect(crack2.find_b_subkey).to eq([3, 30, 57, 84])
+  end
+
+  xit 'can find all possible options for the c subkey' do
+    expect(crack2.find_c_subkey).to eq([5, 32, 59, 86])
+  end
+
+  xit 'can find all possible options for the d subkey' do
+    expect(crack2.find_d_subkey).to eq([1, 28, 55, 82])
+  end
+
+  xit 'can turn the possible keys into strings and rjust them' do
+    expect(crack2.stringify_them("a")).to eq(["20", "47", "74"])
+    expect(crack2.stringify_them("b")).to eq(["03", "30", "57", "84"])
+    expect(crack2.stringify_them("c")).to eq(["05", "32", "59", "86"])
+    expect(crack2.stringify_them("d")).to eq(["01", "28", "55", "82"])
+  end
+
+  xit 'can find the pieces of the key' do
+    expect(crack2.figure_out_key).to eq("20328")
+  end
+
+  it 'can decrypt a message with no key provided' do
+    expect(crack2.decrypt_message("ipyqpkvstddzddawt!mmpgmfspmcpemyponc?krse", crack2.figure_out_key, "2022-11-12")).to eq("hello instructors! how are you today? end")
+
   end
 
 end
